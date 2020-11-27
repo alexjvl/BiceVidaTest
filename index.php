d54e6258ae5ed109af3693881f9f3a95ed9b4920
Alex <alex@correo.com>
2020-11-27 18:03:09 -0300

    nueva version

--- index.php
+++ index.php
@@ -6,11 +6,7 @@
     <title>Consumo de API BICEVIDA</title>
 </head>
 <body>
-    <form class="" action="" method="get">
-        <label for="insurance"></label>
-        <input type="text" name="insurance" value="">
-        <button type="submit" name="button">Enviar</button>
-    </form>
+    
 </body>
 </html>
 
@@ -18,8 +14,8 @@
 
  
 
-//echo "Prueba API BICEVIDA"
-$url="https://my-primera-api.firebaseio.com/insurance.json";
+//url donde se encuentra archivo json para test de API BICEVIDA
+$url="https://api-bicevida-test.firebaseio.com/insurance.json";
 // traigo desde bd firebaseio y lo guardo en arreglo datos 
 $json= file_get_contents($url);
 $datos= json_decode ($json,true);
@@ -28,10 +24,6 @@ $imagen = $datos ["image"];
 $nomseguro = $datos ["name"];
 $precio = $datos ["price"];
 
-//$imagen = imagecreatefrompng ('https://ventaenlinea.bicevida.cl/pub/media/catalog/product/cache/69eb2560c3d44c78f7327201dc5a282b/i/m/img-01.jpg');
-//header ('Content-Type:image/png');
-//imagepng ($imagen);
-//imagedestroy ($imagen);
 
 echo "Seguros BICEVIDA";
 echo "<br>";
